60c675467d96aae108f7945c7df65479f2010d7d
Devansh <devanshagarwal788@gmail.com>
2025-02-04 20:41:42 +0530

    task completed

--- public/script.js
+++ public/script.js
@@ -1,1 +1,102 @@
-// Write your code from here!!
+let questions = [];
+
+async function apiCall() {
+  try {
+    const res = await fetch("http://localhost:3000/question");
+    questions = await res.json();
+  } catch (err) {
+    console.error("Error fetching data:", err);
+  }
+}
+
+async function fetchData() {
+  await apiCall();
+  xyz();
+}
+
+function xyz(){
+    const ques = document.getElementById("ques");
+    const ansBtn = document.getElementById("ansBtns");
+    const nxtBtn = document.getElementById("nxtBtn");
+  
+    let currentQuesIdx = 0;
+    let score = 0;
+  
+    function strt() {
+      currentQuesIdx = 0;
+      score = 0;
+      nxtBtn.innerHTML = "Next";
+      showQues();
+    }
+    function showQues() {
+      resetState();
+      let currQues = questions[currentQuesIdx];
+      let quesNum = currentQuesIdx + 1;
+      ques.innerHTML = quesNum + " . " + currQues.question;
+  
+      currQues.options.forEach(option => {
+        const button = document.createElement("button");
+        button.innerHTML = option;
+        button.classList.add("btn");
+        ansBtn.appendChild(button);
+        if (option === currQues.answer) {
+            button.dataset.correct = "true";
+        }
+        button.addEventListener("click", selectAns);
+    });
+
+}
+  
+    function resetState() {
+      nxtBtn.style.display = "none";
+      while (ansBtn.firstChild) {
+        ansBtn.removeChild(ansBtn.firstChild);
+      }
+    }
+  
+    function selectAns(e) {
+      const selectedBtn = e.target;
+      const isCorrect = selectedBtn.dataset.correct === "true";
+      if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+      } else {
+        selectedBtn.classList.add("incorrect");
+      }
+      Array.from(ansBtn.children).forEach((button) => {
+        if (button.dataset.correct === "true") {
+          button.classList.add("correct");
+        }
+        button.disabled = true;
+      });
+      nxtBtn.style.display = "block";
+    }
+  
+    nxtBtn.addEventListener("click", () => {
+      if (currentQuesIdx < questions.length) {
+        handleNxtBtn();
+      } else {
+        strt();
+      }
+    });
+  
+    function handleNxtBtn() {
+      currentQuesIdx++;
+      if (currentQuesIdx < questions.length) {
+        showQues();
+      } else {
+        showScore();
+      }
+    }
+  
+    function showScore() {
+      resetState();
+      ques.innerHTML = `You scored ${score} out of ${questions.length}!`;
+      nxtBtn.innerHTML = "Play Again";
+      nxtBtn.style.display = "block";
+    }
+  
+    strt();
+}
+
+fetchData();
--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 // Write your server logic here
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.url == "/question") {
+    fs.readFile("questions.json", (err, data) => {
+      if (err) {
+        res.end("Internal Server Error");
+      } else {
+        res.end(data);
+      }
+    });
+  } else {
+    res.end("invaild request");
+  }
+});
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
